07d799e8f05324e8bf99a9e4826028301f61efba
Brandon Shar <brandon.shar@icloud.com>
2021-03-02 11:29:56 -0500

    Add computed values

--- src/Inputs.js
+++ src/Inputs.js
@@ -2,17 +2,24 @@ import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 import { useQueryString, useLocalStorage, useDebounceEffect } from '@bellawatt/react-hooks'
 import InputContext from './context'
+import { omit } from './helpers';
 
-const Inputs = ({defaults, children, options, watch = {}}) => {
+const Inputs = ({defaults, children, options, watch = {}, computed = {}}) => {
   const { debounceDelay = 500, localStorageName = 'inputs' } = options || {}
 
   const [urlInputs, updateQueryString] = useQueryString()
   const [localInputs, setLocalInputs] = useLocalStorage(localStorageName)
   const hasUrlInputs = Object.keys(urlInputs).length > 0
+  const computedParams = Object.keys(computed);
+
+  const getComputedValues = (originalObj) => computedParams.reduce((fields, key) => {
+    return {...fields, [key]: computed[key]({...originalObj})};
+  }, {});
 
   const [inputs, setInputs] = useState({
     ...defaults,
     ...(hasUrlInputs ? urlInputs : localInputs),
+    ...getComputedValues({...defaults, ...(hasUrlInputs ? urlInputs : localInputs)}),
   })
 
   const setInput = obj => {
@@ -21,14 +28,18 @@ const Inputs = ({defaults, children, options, watch = {}}) => {
 
       return {...changes, ...(watch[key]({...inputs, ...obj, ...changes}))}
     }, {});
-    
-    setInputs(current => ({...current, ...obj, ...watcherChanges}))
+
+    const objWithWatcher = {...obj, ...watcherChanges};
+
+    const computedValues = getComputedValues(objWithWatcher);
+
+    setInputs(current => ({...current, ...objWithWatcher, ...computedValues}))
   }
 
   useDebounceEffect(
     () => {
-      updateQueryString(inputs)
-      setLocalInputs(inputs)
+      updateQueryString(omit(computedParams, inputs))
+      setLocalInputs(omit(computedParams, inputs))
     },
     debounceDelay,
     [inputs]
